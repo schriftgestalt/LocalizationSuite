26adf81fdffef3648199363f7bd1f43ed5eca816
Peter Kraml <piets@boinx.com>
2013-08-01 13:39:52 +0200

    changed colors of diff

--- LocTools/Source/Framework/Matching/NSString+DiffTools.m
+++ LocTools/Source/Framework/Matching/NSString+DiffTools.m
@@ -27,51 +27,68 @@
     // make the text appear in blue
     for (LTDifference *diff in differences)
 	{
-		NSString *stringToAppend = diff.newValue;
-		
-		if (diff.type == LTDifferenceDelete)
-		{
-			stringToAppend = diff.oldValue;
+		if (diff.type == LTDifferenceAdd)
+        {
+			//String was added, append it, color it green
+			[self addGreenString:diff.newValue toAttributedString:attrString];
 		}
-		
-        [attrString appendAttributedString:[[NSAttributedString alloc] initWithString:stringToAppend]];
-		
-		NSRange currentRange = NSMakeRange(attrString.length-stringToAppend.length, stringToAppend.length);
-		
-        NSColor *color = [NSColor blackColor];
-		NSColor *backgroundColor = [NSColor clearColor];
-		
-		
-        if (diff.type == LTDifferenceAdd)
-        {
-            color = [NSColor colorWithCalibratedRed:0.f green:0.6f blue:0.f alpha:1.000];
-			backgroundColor = [NSColor colorWithCalibratedRed:0.72f green:1.f blue:0.72f alpha:1];
-        }
-        else if (diff.type == LTDifferenceDelete)
-        {
-            color = [NSColor colorWithCalibratedRed:0.6f green:0.f blue:0.f alpha:1.000];
-			backgroundColor = [NSColor colorWithCalibratedRed:1.f green:0.72f blue:0.72f alpha:1];
-			
-			[attrString addAttribute:NSStrikethroughStyleAttributeName
-							   value:[NSNumber numberWithInt:1]
-							   range:currentRange];
-        }
+		else if (diff.type == LTDifferenceDelete)
+		{
+			//String was removed, append the old value and color it red
+			[self addRedString:diff.oldValue toAttributedString:attrString];
+		}
 		else if (diff.type == LTDifferenceChange)
 		{
-			color = [NSColor colorWithCalibratedRed:0.f green:0.f blue:0.6f alpha:1.000];
-			backgroundColor = [NSColor colorWithCalibratedRed:0.72f green:0.72f blue:1.f alpha:1];
+			//string was changed, append the old and the new one, color accordingly
+			[self addRedString:diff.oldValue toAttributedString:attrString];
+			[self addGreenString:diff.newValue toAttributedString:attrString];
 		}
-		
-        [attrString addAttribute:NSForegroundColorAttributeName value:color range:currentRange];
-		[attrString addAttribute:NSBackgroundColorAttributeName value:backgroundColor range:currentRange];
-		
-		//Append space
-		[attrString appendAttributedString:[[NSAttributedString alloc] initWithString:@" "]];
+		else
+		{
+			//no change, simply append it.
+			[attrString appendAttributedString:[[NSAttributedString alloc] initWithString:diff.newValue]];
+			[attrString appendAttributedString:[[NSAttributedString alloc] initWithString:@" "]];
+		}
     }
 	
     [attrString endEditing];
 	
     return [[NSAttributedString alloc] initWithAttributedString:attrString];
+}
+
+- (void)addGreenString:(NSString *)newString toAttributedString:(NSMutableAttributedString *)attrString
+{
+	[attrString appendAttributedString:[[NSAttributedString alloc] initWithString:newString]];
+	
+	NSRange currentRange = NSMakeRange(attrString.length-newString.length, newString.length);
+	
+	NSColor *color = [NSColor colorWithCalibratedRed:0.f green:0.6f blue:0.f alpha:1.000];
+	NSColor *backgroundColor = [NSColor colorWithCalibratedRed:0.72f green:1.f blue:0.72f alpha:1];
+	
+	[attrString addAttribute:NSForegroundColorAttributeName value:color range:currentRange];
+	[attrString addAttribute:NSBackgroundColorAttributeName value:backgroundColor range:currentRange];
+	
+	[attrString appendAttributedString:[[NSAttributedString alloc] initWithString:@" "]];
+}
+
+- (void)addRedString:(NSString *)oldString toAttributedString:(NSMutableAttributedString *)attrString
+{
+	[attrString appendAttributedString:[[NSAttributedString alloc] initWithString:oldString]];
+	
+	NSRange currentRange = NSMakeRange(attrString.length-oldString.length, oldString.length);
+	
+	NSColor *color = [NSColor colorWithCalibratedRed:0.6f green:0.f blue:0.f alpha:1.000];
+	NSColor *backgroundColor = [NSColor colorWithCalibratedRed:1.f green:0.72f blue:0.72f alpha:1];
+	
+	//strikethrough
+	[attrString addAttribute:NSStrikethroughStyleAttributeName
+					   value:[NSNumber numberWithInt:1]
+					   range:currentRange];
+	
+	[attrString addAttribute:NSForegroundColorAttributeName value:color range:currentRange];
+	[attrString addAttribute:NSBackgroundColorAttributeName value:backgroundColor range:currentRange];
+	
+	[attrString appendAttributedString:[[NSAttributedString alloc] initWithString:@" "]];
 }
 
 
