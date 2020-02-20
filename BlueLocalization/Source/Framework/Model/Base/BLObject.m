87b7856703e31b11cc8350de777c41258adafa7f
schriftgestalt <georg.seifert@mac.com>
2020-02-20 10:14:29 +0100

    clang format

--- BlueLocalization/Source/Framework/Model/Base/BLObject.m
+++ BlueLocalization/Source/Framework/Model/Base/BLObject.m
@@ -10,382 +10,342 @@
 
 #import "BLFileInternal.h"
 
-NSString *BLObjectReferenceChangedKey	= @"<reference>";
+NSString *BLObjectReferenceChangedKey = @"<reference>";
 
-NSString *BLObjectFiletypeUnknownError  = @"File type unknown";
-NSString *BLObjectFileNotFoundError		= @"File has not been found";
+NSString *BLObjectFiletypeUnknownError = @"File type unknown";
+NSString *BLObjectFileNotFoundError = @"File has not been found";
 NSString *BLObjectFileUnimportableError = @"File cannot be imported";
-
 
 @implementation BLObject
 
 #pragma mark - Initializers
 
-- (id)init
-{
-    self = [super init];
-    
+- (id)init {
+	self = [super init];
+
 	if (self) {
-		_changeDate = [[NSDate alloc] initWithTimeIntervalSinceReferenceDate: 0];
+		_changeDate = [[NSDate alloc] initWithTimeIntervalSinceReferenceDate:0];
 		_changedValues = [[NSMutableArray alloc] init];
 		_flags = 0;
-		
+
 		_errors = [[NSMutableArray alloc] init];
 	}
 
-    return self;
+	return self;
 }
-
 
 #pragma mark - Serialization
 
-- (id)initWithPropertyList:(NSDictionary *)plist
-{
-    self = [self init];
-    
+- (id)initWithPropertyList:(NSDictionary *)plist {
+	self = [self init];
+
 	if (self) {
-		[self setChangeDate: [plist objectForKey: BLFileChangeDateKey]];
-		[self setFlags: [[plist objectForKey: BLFileFlagsKey] unsignedIntValue]];
-		[self setErrors: [plist objectForKey: BLFileErrorsKey]];
-		
-		[_changedValues setArray: [plist objectForKey: BLFileChangedValuesKey]];
+		[self setChangeDate:[plist objectForKey:BLFileChangeDateKey]];
+		[self setFlags:[[plist objectForKey:BLFileFlagsKey] unsignedIntValue]];
+		[self setErrors:[plist objectForKey:BLFileErrorsKey]];
+
+		[_changedValues setArray:[plist objectForKey:BLFileChangedValuesKey]];
 	}
-    
-    return self;
+
+	return self;
 }
 
-- (NSDictionary *)propertyListWithAttributes:(NSDictionary *)attributes
-{
-    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-	
+- (NSDictionary *)propertyListWithAttributes:(NSDictionary *)attributes {
+	NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+
 	[dict secureSetObject:[self changeDate] forKey:BLFileChangeDateKey];
 	[dict secureSetObject:[self errors] forKey:BLFileErrorsKey];
-	[dict secureSetObject:[NSNumber numberWithUnsignedInteger: [self flags]] forKey:BLFileFlagsKey];
-	
-	if (![[attributes objectForKey: BLClearChangeInformationSerializationKey] boolValue])
-		[dict secureSetObject:[self changedValues] forKey:BLFileChangedValuesKey];
-    
-    return dict;
-}
+	[dict secureSetObject:[NSNumber numberWithUnsignedInteger:[self flags]] forKey:BLFileFlagsKey];
 
+	if (![[attributes objectForKey:BLClearChangeInformationSerializationKey] boolValue])
+		[dict secureSetObject:[self changedValues] forKey:BLFileChangedValuesKey];
+
+	return dict;
+}
 
 #pragma mark - NSCopying Implementation
 
-- (id)copyWithZone:(NSZone *)zone
-{
-    BLObject *newObject;
-    
-    newObject = [[[self class] alloc] initWithPropertyList: [self propertyListWithAttributes: nil]];
-    
-    return newObject;
+- (id)copyWithZone:(NSZone *)zone {
+	BLObject *newObject;
+
+	newObject = [[[self class] alloc] initWithPropertyList:[self propertyListWithAttributes:nil]];
+
+	return newObject;
 }
 
 #pragma mark - Change Tracking
 
-- (NSArray *)changedValues
-{
-    return _changedValues;
+- (NSArray *)changedValues {
+	return _changedValues;
 }
 
-- (NSString *)changeDescription
-{
+- (NSString *)changeDescription {
 	if ([[self errors] count]) {
-		return [[self errors] componentsJoinedByString: @", "];
-	} else {
-		NSMutableArray *changes = [NSMutableArray arrayWithArray: [self changedValues]];
+		return [[self errors] componentsJoinedByString:@", "];
+	}
+	else {
+		NSMutableArray *changes = [NSMutableArray arrayWithArray:[self changedValues]];
 		NSString *description = @"";
-		
+
 		// Reference changed
-		if ([changes containsObject: BLObjectReferenceChangedKey]) {
-			description = NSLocalizedStringFromTableInBundle(@"Reference", @"Localizable", [NSBundle bundleForClass: [self class]], nil);
-			[changes removeObject: BLObjectReferenceChangedKey];
+		if ([changes containsObject:BLObjectReferenceChangedKey]) {
+			description = NSLocalizedStringFromTableInBundle(@"Reference", @"Localizable", [NSBundle bundleForClass:[self class]], nil);
+			[changes removeObject:BLObjectReferenceChangedKey];
 		}
 		// Languages changed
 		if ([changes count]) {
 			if ([description length])
-				description = [description stringByAppendingString: @", "];
-			description = [description stringByAppendingString: [[changes valueForKey: @"languageDescription"] componentsJoinedByString: @", "]];
+				description = [description stringByAppendingString:@", "];
+			description = [description stringByAppendingString:[[changes valueForKey:@"languageDescription"] componentsJoinedByString:@", "]];
 		}
-		
+
 		return description;
 	}
 }
 
-+ (NSSet *)keyPathsForValuesAffectingChangeDescription
-{
-	return [NSSet setWithObjects: @"changedValues", @"errors", nil];
++ (NSSet *)keyPathsForValuesAffectingChangeDescription {
+	return [NSSet setWithObjects:@"changedValues", @"errors", nil];
 }
 
-- (void)setChangedValues:(NSArray *)array
-{
-    [_changedValues setArray: array];
-    [[self parentObject] addChangedValues: array];
+- (void)setChangedValues:(NSArray *)array {
+	[_changedValues setArray:array];
+	[[self parentObject] addChangedValues:array];
 }
 
-- (void)addChangedValues:(NSArray *)array
-{
-    for (NSUInteger i=0; i<[array count]; i++)
-        [self setValue:[array objectAtIndex: i] didChange:YES];
+- (void)addChangedValues:(NSArray *)array {
+	for (NSUInteger i = 0; i < [array count]; i++)
+		[self setValue:[array objectAtIndex:i] didChange:YES];
 }
 
-- (void)setValue:(NSString *)key didChange:(BOOL)changed
-{
-	[self willChangeValueForKey: @"changedValues"];
-	
-    if (changed) {
-		if (![_changedValues containsObject: key])
-			[_changedValues addObject: key];
-    } else {
-		if ([_changedValues containsObject: key])
-			[_changedValues removeObject: key];
-		for (NSUInteger i=0; i<[[self objects] count]; i++)
-			[[[self objects] objectAtIndex: i] setValue:key didChange:NO];
-    }
+- (void)setValue:(NSString *)key didChange:(BOOL)changed {
+	[self willChangeValueForKey:@"changedValues"];
+
+	if (changed) {
+		if (![_changedValues containsObject:key])
+			[_changedValues addObject:key];
+	}
+	else {
+		if ([_changedValues containsObject:key])
+			[_changedValues removeObject:key];
+		for (NSUInteger i = 0; i < [[self objects] count]; i++)
+			[[[self objects] objectAtIndex:i] setValue:key didChange:NO];
+	}
 	[[self parentObject] setObjectValue:key didChange:changed];
-	
-	[self didChangeValueForKey: @"changedValues"];
+
+	[self didChangeValueForKey:@"changedValues"];
 }
 
-- (void)setObjectValue:(NSString *)key didChange:(BOOL)changed
-{
-	[self willChangeValueForKey: @"changedValues"];
-    if (changed) {
+- (void)setObjectValue:(NSString *)key didChange:(BOOL)changed {
+	[self willChangeValueForKey:@"changedValues"];
+	if (changed) {
 		[self setValue:key didChange:YES];
-	} else {
+	}
+	else {
 		changed = NO;
-		for (NSUInteger i=0; i<[[self objects] count]; i++) {
-			if ((changed = [[[self objects] objectAtIndex: i] valueDidChange: key]))
+		for (NSUInteger i = 0; i < [[self objects] count]; i++) {
+			if ((changed = [[[self objects] objectAtIndex:i] valueDidChange:key]))
 				break;
 		}
-		if (!changed && [_changedValues containsObject: key])
-			[_changedValues removeObject: key];
+		if (!changed && [_changedValues containsObject:key])
+			[_changedValues removeObject:key];
 		[[self parentObject] setObjectValue:key didChange:changed];
 	}
-	[self didChangeValueForKey: @"changedValues"];
+	[self didChangeValueForKey:@"changedValues"];
 }
 
-- (void)setNothingDidChange
-{
-    [self willChangeValueForKey: @"changedValues"];
-    [_changedValues removeAllObjects];
-    [self didChangeValueForKey: @"changedValues"];
-    
-    if ([self objects])
-        [[self objects] makeObjectsPerformSelector: @selector(setNothingDidChange)];
+- (void)setNothingDidChange {
+	[self willChangeValueForKey:@"changedValues"];
+	[_changedValues removeAllObjects];
+	[self didChangeValueForKey:@"changedValues"];
+
+	if ([self objects])
+		[[self objects] makeObjectsPerformSelector:@selector(setNothingDidChange)];
 }
 
-- (BOOL)didChange
-{
-    return ([_changedValues count] > 0);
+- (BOOL)didChange {
+	return ([_changedValues count] > 0);
 }
 
-+ (NSSet *)keyPathsForValuesAffectingDidChange
-{
-	return [NSSet setWithObjects: @"changedValues", nil];
++ (NSSet *)keyPathsForValuesAffectingDidChange {
+	return [NSSet setWithObjects:@"changedValues", nil];
 }
 
-- (BOOL)valueDidChange:(NSString *)key
-{
-	return [_changedValues containsObject: key];
+- (BOOL)valueDidChange:(NSString *)key {
+	return [_changedValues containsObject:key];
 }
 
-- (BOOL)referenceChanged
-{
-	return [self valueDidChange: BLObjectReferenceChangedKey];
+- (BOOL)referenceChanged {
+	return [self valueDidChange:BLObjectReferenceChangedKey];
 }
 
-- (void)setReferenceChanged:(BOOL)changed
-{
+- (void)setReferenceChanged:(BOOL)changed {
 	[self setValue:BLObjectReferenceChangedKey didChange:changed];
 }
 
-+ (NSSet *)keyPathsForValuesAffectingReferenceChanged
-{
-	return [NSSet setWithObjects: @"changedValues", nil];
++ (NSSet *)keyPathsForValuesAffectingReferenceChanged {
+	return [NSSet setWithObjects:@"changedValues", nil];
 }
 
-- (NSArray *)changedLanguages
-{
+- (NSArray *)changedLanguages {
 	NSMutableArray *languages = [NSMutableArray array];
-	
+
 	for (NSString *value in self.changedValues) {
-		if ([value isEqual: BLObjectReferenceChangedKey])
+		if ([value isEqual:BLObjectReferenceChangedKey])
 			continue;
-		if ([BLLanguageTranslator isLanguageIdentifier: value])
-			[languages addObject: value];
+		if ([BLLanguageTranslator isLanguageIdentifier:value])
+			[languages addObject:value];
 	}
-	
+
 	return languages;
 }
 
-- (void)setChangedLanguages:(NSArray *)languages
-{
-	NSMutableArray *newChangedValues = [NSMutableArray arrayWithArray: _changedValues];
-	[newChangedValues removeObjectsInArray: self.changedLanguages];
-	[newChangedValues addObjectsFromArray: languages];
-	
+- (void)setChangedLanguages:(NSArray *)languages {
+	NSMutableArray *newChangedValues = [NSMutableArray arrayWithArray:_changedValues];
+	[newChangedValues removeObjectsInArray:self.changedLanguages];
+	[newChangedValues addObjectsFromArray:languages];
+
 	// Remove values
-	for (NSString *key in [NSArray arrayWithArray: _changedValues]) {
-		if (![newChangedValues containsObject: key])
+	for (NSString *key in [NSArray arrayWithArray:_changedValues]) {
+		if (![newChangedValues containsObject:key])
 			[self setValue:key didChange:NO];
 	}
-	
+
 	// Add values
 	for (NSString *key in newChangedValues) {
-		if (![_changedValues containsObject: key])
+		if (![_changedValues containsObject:key])
 			[self setValue:key didChange:YES];
 	}
 }
 
-+ (NSSet *)keyPathsForValuesAffectingChangedLanguages
-{
-	return [NSSet setWithObjects: @"changedValues", nil];
++ (NSSet *)keyPathsForValuesAffectingChangedLanguages {
+	return [NSSet setWithObjects:@"changedValues", nil];
 }
 
-- (NSDate *)changeDate
-{
-    return _changeDate;
+- (NSDate *)changeDate {
+	return _changeDate;
 }
 
-- (void)setChangeDate:(NSDate *)date
-{
-    _changeDate = date;
+- (void)setChangeDate:(NSDate *)date {
+	_changeDate = date;
 }
 
 #pragma mark - Accessors
 
-- (NSUInteger)flags
-{
-    return _flags;
+- (NSUInteger)flags {
+	return _flags;
 }
 
-- (void)setFlags:(NSUInteger)flags
-{
-    _flags = flags;
+- (void)setFlags:(NSUInteger)flags {
+	_flags = flags;
 }
 
-- (NSString *)description
-{
-    return [NSString stringWithFormat: @"<%@ %p>[%@]", NSStringFromClass([self class]), self, [self name]];
+- (NSString *)description {
+	return [NSString stringWithFormat:@"<%@ %p>[%@]", NSStringFromClass([self class]), self, [self name]];
 }
 
-- (NSString *)name
-{
-    return @"BLObject";
+- (NSString *)name {
+	return @"BLObject";
 }
 
-- (NSArray *)errors
-{
-    return _errors;
+- (NSArray *)errors {
+	return _errors;
 }
 
-- (void)setErrors:(NSArray *)newErrors
-{
-    [_errors setArray: newErrors];
-	[[self parentObject] addObjectErrors: newErrors];
+- (void)setErrors:(NSArray *)newErrors {
+	[_errors setArray:newErrors];
+	[[self parentObject] addObjectErrors:newErrors];
 }
 
-- (void)addObjectErrors:(NSArray *)newErrors
-{
+- (void)addObjectErrors:(NSArray *)newErrors {
 	if ([newErrors count]) {
-		[self willChangeValueForKey: @"errors"];
-		[_errors addObjectsFromArray: newErrors];
-		[self didChangeValueForKey: @"errors"];
-		
-		[[self parentObject] addObjectErrors: newErrors];
-	} else {
+		[self willChangeValueForKey:@"errors"];
+		[_errors addObjectsFromArray:newErrors];
+		[self didChangeValueForKey:@"errors"];
+
+		[[self parentObject] addObjectErrors:newErrors];
+	}
+	else {
 		NSMutableSet *allErrors;
-		
+
 		allErrors = [NSMutableSet set];
-		for (NSUInteger i=0; i<[[self objects] count]; i++)
-			[allErrors addObjectsFromArray: [[[self objects] objectAtIndex: i] errors]];
-		
-		[self setErrors: [allErrors allObjects]];
+		for (NSUInteger i = 0; i < [[self objects] count]; i++)
+			[allErrors addObjectsFromArray:[[[self objects] objectAtIndex:i] errors]];
+
+		[self setErrors:[allErrors allObjects]];
 	}
 }
 
-- (id)valueForUndefinedKey:(NSString *)key
-{
-    return nil;
+- (id)valueForUndefinedKey:(NSString *)key {
+	return nil;
 }
 
 #pragma mark - Scripting
 
-- (BOOL)isActive
-{
-    return !([self flags] & BLObjectDeactivatedFlag);
+- (BOOL)isActive {
+	return !([self flags] & BLObjectDeactivatedFlag);
 }
 
-- (void)setIsActive:(BOOL)flag
-{
-    if (([self flags] & BLObjectDeactivatedFlag) && flag)
-        [self setFlags: [self flags] - BLObjectDeactivatedFlag];
-    if (!([self flags] & BLObjectDeactivatedFlag) && !flag)
-        [self setFlags: [self flags] + BLObjectDeactivatedFlag];
+- (void)setIsActive:(BOOL)flag {
+	if (([self flags] & BLObjectDeactivatedFlag) && flag)
+		[self setFlags:[self flags] - BLObjectDeactivatedFlag];
+	if (!([self flags] & BLObjectDeactivatedFlag) && !flag)
+		[self setFlags:[self flags] + BLObjectDeactivatedFlag];
 }
 
-- (BOOL)wasUpdated
-{
-    return ([self flags] & BLObjectUpdatedFlag) != 0;
+- (BOOL)wasUpdated {
+	return ([self flags] & BLObjectUpdatedFlag) != 0;
 }
 
-- (void)setWasUpdated:(BOOL)flag
-{
-    if (([self flags] & BLObjectUpdatedFlag) && !flag)
-        [self setFlags: [self flags] - BLObjectUpdatedFlag];
-    if (!([self flags] & BLObjectUpdatedFlag) && flag)
-        [self setFlags: [self flags] + BLObjectUpdatedFlag];
+- (void)setWasUpdated:(BOOL)flag {
+	if (([self flags] & BLObjectUpdatedFlag) && !flag)
+		[self setFlags:[self flags] - BLObjectUpdatedFlag];
+	if (!([self flags] & BLObjectUpdatedFlag) && flag)
+		[self setFlags:[self flags] + BLObjectUpdatedFlag];
 }
 
 #pragma mark - Treeing
 
-- (NSArray *)objects
-{
-    return nil;
+- (NSArray *)objects {
+	return nil;
 }
 
-+ (NSSet *)keyPathsForValuesAffectingNumberOfObjects
-{
-	return [NSSet setWithObjects: @"objects", nil];
++ (NSSet *)keyPathsForValuesAffectingNumberOfObjects {
+	return [NSSet setWithObjects:@"objects", nil];
 }
 
-- (id)parentObject
-{
-    return nil;
+- (id)parentObject {
+	return nil;
 }
 
-- (id)rootObject
-{
-    return ([self parentObject]) ? [self parentObject] : self;
+- (id)rootObject {
+	return ([self parentObject]) ? [self parentObject] : self;
 }
 
 #pragma mark - Utilities
 
-- (id)object
-{
+- (id)object {
 	return self;
 }
 
-- (BOOL)isEqual:(id)other
-{
+- (BOOL)isEqual:(id)other {
 	// Compare Classes
-	if (![other isKindOfClass: [self class]])
+	if (![other isKindOfClass:[self class]])
 		return NO;
-	
+
 	// Changes
-	if (![[self changedValues] isEqual: [other changedValues]]) {
-		if (![[NSSet setWithArray: (NSArray *)[self changedValues]] isEqual: [NSSet setWithArray: (NSArray *)[other changedValues]]])
+	if (![[self changedValues] isEqual:[other changedValues]]) {
+		if (![[NSSet setWithArray:(NSArray *)[self changedValues]] isEqual:[NSSet setWithArray:(NSArray *)[other changedValues]]])
 			return NO;
 	}
-	if (![[self changeDate] isEqual: [other changeDate]])
+	if (![[self changeDate] isEqual:[other changeDate]])
 		return NO;
-	
+
 	// Erros, flags
-	if (![[self errors] isEqual: [other errors]])
+	if (![[self errors] isEqual:[other errors]])
 		return NO;
 	if ([self flags] != [other flags])
 		return NO;
-	
+
 	return YES;
 }
 
